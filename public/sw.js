e8ea4935ae1e8f62dfd9c3cd128451fce5a53554
techwithdonet <techwithdonet@gmail.com>
2026-09-10 10:14:16 +0530

    Restore portrait dashboard calendar

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "build-1789013637583";
+const VERSION = "build-1789015375245";
 const CACHE_PREFIX = "mecardee-shell-";
 const CACHE = `mecardee-shell-deploy-${VERSION}`;
 const SHELL = ["/manifest.webmanifest", "/icons/mecardee-192.png", "/icons/mecardee-512.png"];
