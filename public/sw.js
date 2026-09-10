3f928f419789a0d8e3c245c653b8ef9523e16f7a
techwithdonet <techwithdonet@gmail.com>
2026-09-10 09:47:14 +0530

    Finish mobile rental layout fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "build-1788972614083";
+const VERSION = "build-1789013637583";
 const CACHE_PREFIX = "mecardee-shell-";
 const CACHE = `mecardee-shell-deploy-${VERSION}`;
 const SHELL = ["/manifest.webmanifest", "/icons/mecardee-192.png", "/icons/mecardee-512.png"];
